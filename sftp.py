13a08038e131a7462b1add47cac5dfd7478cc866
hocinebib <hocine.meraouna@gmail.com>
2024-12-07 15:02:31 +0100

    added some docstrings

--- sftp.py
+++ sftp.py
@@ -1,30 +1,69 @@
+#!/usr/bin/python3
+
+"""
+
+some messing around about sftp connection with password using python library paramiko
+
+use exemple :
+
+	python3 sftp.py [host] [username] [password] [folder path]
+
+"""
 
 import paramiko
-import os
-
-host = 'eu-central-1.sftpcloud.io'
-user = '4da0c1944ee0490f88543ccb3ade88e3'
-pswd = 'DCm5xIRVuxFL6us2XeXf6lLBT54Bcu7N'
-
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-ssh.connect(host, username=user, password=pswd,allow_agent=False,look_for_keys=False)
+import argparse
 
 
-sftp = ssh.open_sftp()
+def sftp_connection(host, username, password, folder):
+	"""
+	function used for the connection to the SFTP account and to list the files
+	using paramiko library to do so
+	"""
 
-print(sftp.listdir('outgoing'))
+	ssh = paramiko.SSHClient()
+	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-for f in sftp.listdir('outgoing'):
-	if '.' in f:
-		print(f)
+	ssh.connect(host, username=username, password=password,allow_agent=False,look_for_keys=False)
+
+	sftp = ssh.open_sftp()
+
+	print(sftp.listdir(folder))
+
+	for f in sftp.listdir(folder):
+		if '.' in f:
+			print(f)
 
 
-t = sftp.listdir_attr('outgoing')
+	t = sftp.listdir_attr(folder)
 
-a = str(t[0])
+	a = str(t[0])
 
-print(' '.join(a.split()[-4:]))
+	print(' '.join(a.split()[-4:]))
 
-ssh.close()
+	ssh.close()
+
+
+if __name__ == '__main__':
+
+    PARSER = argparse.ArgumentParser()
+
+    PARSER.add_argument("host", help="the SFTP server hostname of IP address", type=str)
+
+    PARSER.add_argument("username", help="the username/login",  type=str)
+
+    PARSER.add_argument("password", help="the password for the authentication", type=str)
+
+    PARSER.add_argument("folder_path", help="the folder in the SFTP server that will be used for the listdir in this test", type=str)
+
+    ARGS = PARSER.parse_args()
+
+    HOST = ARGS.host
+
+    USERNAME = ARGS.username
+
+    PASSWORD = ARGS.password
+
+    FOLDER = ARGS.folder_path
+
+    sftp_connection(HOST, USERNAME, PASSWORD, FOLDER)
+
